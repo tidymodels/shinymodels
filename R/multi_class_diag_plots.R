3c5837f110536bcbbf5769dbe2399e57cb7f9b77
adhikars11 <adhikars@reed.edu>
2021-07-28 11:10:35 -0700

    minor edits

--- R/multi_class_diag_plots.R
+++ R/multi_class_diag_plots.R
@@ -18,7 +18,7 @@ plot_multiclass_obs_pred <-
     dat <- dat %>%
       dplyr::select(-.pred_class) %>%
       tidyr::pivot_longer(
-        cols = starts_with(".pred_"),
+        cols = tidyselect::starts_with(".pred_"),
         names_to = "predicted_class",
         values_to = "predicted_probabilities"
       )
