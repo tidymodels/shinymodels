0f8463b687aa4f58af3487f353307b8b6de5aea7
Julia Silge <julia.silge@gmail.com>
2021-07-30 13:14:15 -0600

    Update docs for data and redocument

--- R/data.R
+++ R/data.R
@@ -1,11 +1,12 @@
 #' A `tune` package result object for internal testing
 #'
 #' @details  An internal object used for development containing the
-#' results of tune_grid(), tune_bayes(), fit_resamples(), or last_fit().
+#' results of [tune::tune_grid()], [tune::tune_bayes()], [tune::fit_resamples()],
+#' or [tune::last_fit()].
 #' @name Internal_data
 #' @aliases ad_data_boost_res mtcars_spline_res
 #' ames_rf_res volcano_res svm_res
 #' @docType data
-#' @return a tune_() result object
+#' @return A tune_() result object
 #' @keywords datasets
 NULL
