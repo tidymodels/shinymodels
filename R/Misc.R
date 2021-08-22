bfb9dc4090398214ffb357470294bc92616c1ecf
adhikars11 <shishamadhikari6@gmail.com>
2021-08-22 11:52:15 -0700

    fixed the performance_object

--- R/Misc.R
+++ R/Misc.R
@@ -155,14 +155,17 @@ quietly_run <- function(expr, warn_pattern = "Ignoring unknown aesthetics") {
 #' @export
 #' @return
 #' A dataframe.
+
 performance_object <- function(x) {
   obj <- x$tune_results %>%
     tune::collect_metrics()
   if (inherits(x$tune_results, "last_fit")) {
-    obj <- dplyr::rename(obj, mean = .estimate) %>%
+    obj <- obj %>%
+      dplyr::rename(mean = .estimate) %>%
       dplyr::select(-.estimator)
   } else {
-    obj  <- dplyr::select(obj, -.estimator, -n, -std_err)
+    obj <- dplyr::select(-.estimator, -n, -std_err)
   }
-  dplyr::relocate(obj, metric = .metric, estimate = mean)
+  obj %>%
+    dplyr::relocate(metric = .metric, estimate = mean)
 }
